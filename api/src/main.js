67fee52e074abc861e0a9a1e7677aaca46902af9
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2022-06-12 15:59:59 +0200

    [API] get tasks

--- api/src/main.js
+++ api/src/main.js
@@ -2,15 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-/* How to make an endpoint?
+/* How to make an endpoint ?
   - Define a path
-  - Get some params o query from the request object
+  - Get some params o query
   - Make some validation if is necesary
   - Find somthing in the database
-  - Return some json data with the response object
+  - Return a json with some data
 */
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/tasks', (req, res) => {
+
+  const mysql = require('mysql');
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'aszendit'
+  });
+
+  connection.connect();
+
+  connection.query('SELECT * FROM tasks', function (err, rows, fields) {
+    if (err) {
+      console.error(err)
+
+      res.sendStatus(500)
+      return
+    };
+
+    const json = rows.map(r => {
+      r.done = r.done === 1
+      return r
+    })
+
+    res.send(json)
+  });
+
+  connection.end();
 })
 
 app.listen(port, () => {
